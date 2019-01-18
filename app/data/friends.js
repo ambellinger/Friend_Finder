39426f73aedceab22492dd415c68df0b288c0116
Lexi <ambellinger24@gmail.com>
2019-01-18 16:21:52 -0600

    added friends, need to add pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,96 @@
 var friends = [
     {
-        "name": "Ahmed",
+        "name": "Koko",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            1,
+            4,
+            5,
+            1,
+            2,
+            5,
+            1,
+            1
+        ]
+
+    },
+
+    {
+        "name": "Payton",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            3,
+            4,
+            4,
+            1,
+            1,
+            2,
+            2,
+            4,
+            1
+        ]
+
+    },
+
+    {
+        "name": "Jack",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            3,
+            1,
+            2,
+            4,
+            4,
+            5
+        ]
+
+    },
+
+    {
+        "name": "Lady",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            2,
+            4,
+            1,
+            5,
+            4,
+            2,
+            1,
+            4,
+            2
+        ]
+
+    },
+
+    {
+        "name": "Owwie",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            4,
+            5,
+            5,
+            2,
+            5,
+            4,
+            5
+        ]
+
+    },
+
+    {
+        "name": "Charmander",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
             5,
@@ -8,14 +98,69 @@ var friends = [
             4,
             4,
             5,
-            1,
+            4,
             2,
             5,
+            5,
+            2
+        ]
+
+    },
+
+    {
+        "name": "Remo",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            1,
+            1,
+            2,
+            1,
             4,
             1
         ]
 
-    }
+    },
+
+    {
+        "name": "Malo",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            5,
+            2,
+            2,
+            5,
+            4,
+            5
+        ]
+
+    },
+
+    {
+        "name": "Cujo",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            1,
+            4,
+            4,
+            4,
+            1,
+            2,
+            1,
+            4,
+            1
+        ]
+
+    },
+    
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
