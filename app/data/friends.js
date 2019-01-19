67db1a581b981dac6d1ad0163d569f9f439621eb
Lexi <ambellinger24@gmail.com>
2019-01-18 19:50:32 -0600

    got the friend difference thing to work

--- app/data/friends.js
+++ app/data/friends.js
@@ -147,15 +147,15 @@ var friends = [
         "name": "Cujo",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            2,
             1,
-            4,
-            4,
-            4,
             1,
-            2,
             1,
-            4,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,22 +2,50 @@
 
 var friends = require("../data/friends");
 
+//Array Sort
+var arraySort = require('array-sort');
+
 //Routing
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         var user = req.body;
         var usersScores = user.scores;
 
+
+        var Totaldifference = [];
+        var results = 0;
+        totalResult = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+
+            results=0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                results += Math.abs(parseInt(usersScores[j]) - parseInt(friends[i].scores[j]));
+
+                
+               // console.log("total results" + friendResult);
+            }
+            Totaldifference.push({ name: friends[i].name, photo: friends[i].photo, friendResult: Math.abs(results) });
+            console.log("results" + results);
+        }
+       
+                
         console.log(usersScores);
         console.log(user);
-       friends.push(user);
-    })
+        friends.push(user);
+        
+      /////THE COMPARE LOOP ISN'T WORKING///
+      arraySort(Totaldifference, 'friendResult');
+      console.log("Closest match:" + Totaldifference[0].name);
 
+        return res.json(Totaldifference[0]);
+    })
 
 }
