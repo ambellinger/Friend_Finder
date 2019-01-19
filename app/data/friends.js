d9341409798c1e17c0a56492db7d0056828375aa
Lexi <ambellinger24@gmail.com>
2019-01-19 17:59:19 -0600

    added the questions, need to make the modal prettier

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,89 +1,89 @@
 var friends = [
     {
         "name": "Koko",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/17353601_214071042406729_6225125509036451616_n.jpg?_nc_cat=100&_nc_ht=scontent-ort2-1.xx&oh=b0a63f8efeadceb3f9ddc54eeeb28228&oe=5CBE7748",
         "scores": [
             5,
-            1,
-            1,
-            4,
             5,
-            1,
             2,
             5,
+            2,
+            2,
+            3,
             1,
-            1
+            1,
+            4
         ]
 
     },
 
     {
         "name": "Payton",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/46522660_518521888628308_1270138854401114112_n.jpg?_nc_cat=108&_nc_ht=scontent-ort2-1.xx&oh=5e0c0bca93ca6eeb38177c6b4756e71a&oe=5CB61843",
         "scores": [
-            2,
             3,
-            4,
-            4,
-            1,
-            1,
-            2,
-            2,
-            4,
-            1
-        ]
-
-    },
-
-    {
-        "name": "Jack",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
             3,
-            1,
-            2,
-            4,
-            4,
-            5
-        ]
-
-    },
-
-    {
-        "name": "Lady",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
             5,
-            2,
-            4,
-            1,
             5,
             4,
-            2,
+            5,
+            5,
+            5,
             1,
-            4,
             2
         ]
 
     },
 
     {
-        "name": "Owwie",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Jack",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/31944783_389859174827914_1720905198353776640_n.jpg?_nc_cat=105&_nc_ht=scontent-ort2-1.xx&oh=b497f2b5643d5d77b381df5f2f71385e&oe=5CBD87A1",
         "scores": [
             5,
-            5,
-            5,
+            4,
             4,
             5,
+            5,
+            3,
+            1,
+            1,
+            1,
+            3
+        ]
+
+    },
+
+    {
+        "name": "Lady",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/50279685_551245538689276_5330648816334929920_n.jpg?_nc_cat=108&_nc_ht=scontent-ort2-1.xx&oh=1bceb16998e077064f322ae32f237221&oe=5CCFBC27",
+        "scores": [
             5,
             2,
             5,
+            5,
+            1,
+            1,
+            5,
+            5,
+            1,
+            5
+        ]
+
+    },
+
+    {
+        "name": "Owwie",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-0/p206x206/23319528_315854072228425_4124482930781579069_n.jpg?_nc_cat=103&_nc_ht=scontent-ort2-1.xx&oh=9e0dd7e3b4af85302f6843f572b09d1d&oe=5CB68052",
+        "scores": [
+            2,
+            1,
+            3,
+            5,
+            5,
             4,
+            5,
+            5,
+            5,
             5
         ]
 
@@ -91,16 +91,52 @@ var friends = [
 
     {
         "name": "Charmander",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/50396411_551245052022658_584070747157168128_n.jpg?_nc_cat=110&_nc_ht=scontent-ort2-1.xx&oh=106eb84380f7dad2ca35c3fbbc9cbca9&oe=5CD1E859",
         "scores": [
+            2,
+            4,
+            1,
+            5,
+            3,
+            3,
             5,
             1,
+            1,
+            4
+        ]
+
+    },
+
+    {
+        "name": "Remo",
+        "photo": "https://photos.app.goo.gl/QF7KhyJRYXMfV2oz7",
+        "scores": [
+            5,
             4,
-            4,
+            5,
             5,
             4,
             2,
+            3,
+            1,
             5,
+            5
+        ]
+
+    },
+
+    {
+        "name": "Malo",
+        "photo": "https://photos.app.goo.gl/cDycqDRcHPd1i9Pj7",
+        "scores": [
+            3,
+            5,
+            5,
+            5,
+            3,
+            3,
+            4,
+            1,
             5,
             2
         ]
@@ -108,53 +144,35 @@ var friends = [
     },
 
     {
-        "name": "Remo",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Cujo",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/46804835_522114524935711_4980693296213917696_n.jpg?_nc_cat=100&_nc_ht=scontent-ort2-1.xx&oh=b8df4f46ffdd88cd9de6b990cfe3e4aa&oe=5CBCCF7E",
         "scores": [
             5,
-            1,
-            4,
-            4,
-            1,
-            1,
-            2,
-            1,
-            4,
-            1
-        ]
-
-    },
-
-    {
-        "name": "Malo",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            1,
-            1,
-            4,
             4,
             5,
-            2,
-            2,
             5,
-            4,
+            5,
+            1,
+            1,
+            3,
+            5,
             5
         ]
 
     },
 
     {
-        "name": "Cujo",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Nori",
+        "photo": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/19510326_264258320721334_7308585909161425822_n.jpg?_nc_cat=103&_nc_ht=scontent-ort2-1.xx&oh=5f05f979f9e39cbb4063ad993339fc24&oe=5CD12C3E",
         "scores": [
             1,
+            5,
+            1,
+            5,
             1,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
+            5,
+            3,
             1,
             1
         ]
